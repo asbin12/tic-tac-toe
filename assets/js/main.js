d506dbb78917f27912de73d0f2129225205549bb
asbin12 <140784814+asbin12@users.noreply.github.com>
2023-12-22 16:28:20 +0545

    v2 of tic tac toe with update ui and line animation for wining

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,28 +3,45 @@ const resetBtn = document.querySelector("#reset-btn");
 const newGameBtn = document.querySelector("#new-btn");
 const msgContainer = document.querySelector(".msg-container");
 const msg = document.querySelector(".msg");
+const strike = document.getElementById("strike");
 
 let turnO = true; //player x player y
 
 const winingPattern = [
-  [0, 1, 2],
-  [0, 3, 6],
-  [0, 4, 8],
-  [1, 4, 7],
-  [2, 5, 8],
-  [2, 4, 6],
-  [3, 4, 5],
-  [6, 7, 8],
+  // [0, 1, 2, 1, 3, 0],
+  // [0, 3, 6, -5, 9, 90],
+  // [0, 4, 8, 1, 9, 45],
+  // [1, 4, 7, 1, 9, 90],
+  // [2, 5, 8, 7.2, 9, 90],
+  // [2, 4, 6, 1, 9, 135],
+  // [3, 4, 5, 1, 9, 0],
+  // [6, 7, 8, 1, 15.2, 0],
+  // [1,2,3,strikeClass="strike-row-1"]
+  // row
+  { numb: [0, 1, 2], strikeClass: "strike-row-1" },
+  { numb: [3, 4, 5], strikeClass: "strike-row-2" },
+  { numb: [6, 7, 8], strikeClass: "strike-row-3" },
+  // column
+  { numb: [0, 3, 6], strikeClass: "strike-col-1" },
+  { numb: [1, 4, 7], strikeClass: "strike-col-2" },
+  { numb: [2, 5, 8], strikeClass: "strike-col-3" },
+  // diagonal
+  { numb: [0, 4, 8], strikeClass: "strike-diagonal-1" },
+  { numb: [2, 4, 6], strikeClass: "strike-diagonal-2" },
 ];
 
 const resetGame = () => {
   turnO = true;
   enableBoxes();
   msgContainer.classList.add("hide");
+  // strike.classList.remove(strikeClass);
+  if (currentStrikeClass) {
+    strike.classList.remove(currentStrikeClass);
+  }
 };
 boxes.forEach((box) => {
   box.addEventListener("click", () => {
-    console.log("boc was clicked");
+    // console.log("boc was clicked");
     if (turnO) {
       box.textContent = "O";
       turnO = false;
@@ -47,21 +64,46 @@ const enableBoxes = () => {
     box.textContent = "";
   }
 };
-const showWinner = (winner) => {
+const showWinner = (winner, strikeClass) => {
   msg.textContent = `Congratulations, Winner is ${winner}`;
   msgContainer.classList.remove("hide");
   disabledBoxes();
+  currentStrikeClass = strikeClass; // Store the current strikeClass
+  strike.classList.add(strikeClass);
 };
 
+// const checkWinner = () => {
+//   for (let pattern of winingPattern) {
+//     let pos1Value = boxes[pattern[0]].textContent;
+//     let pos2Value = boxes[pattern[1]].textContent;
+//     let pos3Value = boxes[pattern[2]].textContent;
+
+//     if (pos1Value != "" && pos2Value != "" && pos3Value != "") {
+//       if (pos1Value === pos2Value && pos2Value === pos3Value) {
+//         showWinner(pos1Value);
+//         document.querySelector(".line").style.width = "16vw";
+//         document.querySelector(
+//           ".line"
+//         ).style.transform = `translate(${pattern[3]}vw,${pattern[4]}vw) rotate(${pattern[5]}deg)`;
+//       }
+//     }
+//   }
+//   if (Array.from(boxes).every((box) => box.textContent !== "")) {
+//     showDraw();
+//   }
+// };
 const checkWinner = () => {
   for (let pattern of winingPattern) {
-    let pos1Value = boxes[pattern[0]].textContent;
-    let pos2Value = boxes[pattern[1]].textContent;
-    let pos3Value = boxes[pattern[2]].textContent;
-
+    // let numb = pattern.numb;
+    // let strikeClass = pattern.strikeClass;
+    let { numb, strikeClass } = pattern;
+    let pos1Value = boxes[numb[0]].textContent;
+    let pos2Value = boxes[numb[1]].textContent;
+    let pos3Value = boxes[numb[2]].textContent;
     if (pos1Value != "" && pos2Value != "" && pos3Value != "") {
       if (pos1Value === pos2Value && pos2Value === pos3Value) {
-        showWinner(pos1Value);
+        showWinner(pos1Value, strikeClass);
+        // strike.classList.toggle(strikeClass);
       }
     }
   }
@@ -69,7 +111,6 @@ const checkWinner = () => {
     showDraw();
   }
 };
-
 const showDraw = () => {
   msg.textContent = "It's a draw!";
   msgContainer.classList.remove("hide");
